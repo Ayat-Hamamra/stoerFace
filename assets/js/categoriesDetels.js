05f53fd1e3636497928e637e1449916ae6e93713
Ayat-Hamamra <ayatmalayshe2003@gmail.com>
2024-10-10 20:24:24 +0300

    Add files via upload

---
+++ assets/js/categoriesDetels.js
@@ -0,0 +1,32 @@
+const getProduct = async () => {
+    const params=new URLSearchParams(window.location.search);
+    const Category=params.get('category');
+    const { data } = await axios.get(`https://dummyjson.com/products/category/${Category}`);
+    return data;
+
+};
+
+const params=new URLSearchParams(window.location.search);
+    const name=params.get('category');
+    document.querySelector(".Products .container h2").textContent=name;
+
+
+
+const displayProducts = async () => {
+    const data = await getProduct();
+    const result = data.products
+    .map( (product)=> {
+        return `
+<div class="product">
+<img src="${product.thumbnail}" alt="${product.description}"/>
+<h2>${product.title}</h2>
+<span>${product.price}</span>
+
+</div>
+`
+    }).join(' ');
+
+    document.querySelector(".Products  .row").innerHTML += result;
+};
+displayProducts();
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,42 @@
 const geCategories = async () => {
     const { data } = await axios.get(
-        "https://dummyjson.com/products/category-list"
+        `https://dummyjson.com/products/category-list`
     );
     return data;
 };
 
 const displayCategories = async (data) => {
     const categories = await geCategories();
-    const result = categories.map((category) => {
-        return `
-<div claas="Category">${category}</div>
+    const result = categories
+        .map((category) => {
+            return `
+<div class="Category">
+<a href='./categoriesDetels.html?category=${category}'>${category}</a>
+</div>
 `;
-    }).join(" ");
+        })
+        .join(" ");
 
-    document.querySelector(".categories  .row").innerHTML = result;
+    document.querySelector(".categories  ").innerHTML += result;
 };
 
 displayCategories();
+const getProducts = async () => {
+    const { data } = await axios.get("https://dummyjson.com/products");
+    return data;
+};
+const displayProducts = async () => {
+    const data = await getProducts();
+    const result = data.products
+    .map( (product)=> {
+        return `
+<div class="product">
+<img src="${product.thumbnail}" alt="${product.description}"/>
+<h2>${product.title}</h2>
+</div>
+`
+    }).join(' ');
+
+    document.querySelector(".Products  .row").innerHTML += result;
+};
+displayProducts();
