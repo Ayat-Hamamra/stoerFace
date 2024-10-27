623385080558803c8aa566ba895598c172e000f3
Ayat-Hamamra <ayatmalayshe2003@gmail.com>
2024-10-27 11:54:03 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@ const displayCategories = async (data) => {
     loader.classList.add("activ");
   try{
     const categories = await geCategories();
+    
     const result = categories
         .map((category) => {
             return `
@@ -33,25 +34,43 @@ const displayCategories = async (data) => {
 
 
 displayCategories();
-const getProducts = async () => {
-    const { data } = await axios.get("https://dummyjson.com/products");
+const getProducts = async (page) => {
+    const Skip=(page-1)*28;
+    const { data } = await axios.get(`https://dummyjson.com/products?limit=28&skip=${Skip}`);
     return data;
 };
-const displayProducts = async () => {
-    const data = await getProducts();
+const displayProducts = async ( page =1) => {
+    const loader= document.querySelector(".loader-container");
+    loader.classList.add("activ");
+  try{
+    const data = await getProducts(page);
+    const numberOfPages=Math.ceil(data.total/28);
     const result = data.products
     .map( (product)=> {
         return `
 <div class="product">
-
 <img src="${product.thumbnail}" alt="${product.description}"/>
-
 <h2>${product.title}</h2>
 </div>
 `
     }).join(' ');
 
-    document.querySelector(".Products  .row").innerHTML += result;
+    document.querySelector(".Products  .row").innerHTML = result;
+
+    let pigination=`<li class="page-item"><a class="page-link" href="#">&laquo;</a></li>`;
+    for(let i=1;i<=numberOfPages;i++){
+        pigination+=` <li class="page-item"><button onclick=displayProducts('${i}') class="page-link">${i}</button></li>`;
+    }
+    pigination+=` <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>`;
+    document.querySelector(".pagination").innerHTML=pigination;
+}
+catch(error){
+ document.querySelector(".categories  ").innerHTML = "<div>error</div>";
+}
+
+finally{
+ loader.classList.remove("activ");
+}
 };
 displayProducts();
  window.onscroll=function(){
